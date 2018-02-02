cf69138c8aa6e4f6f648e7b83330de18dac08a7a
György Lukács <georgelucasletters@gmail.com>
2018-02-02 03:35:53 +0100

    Kesz bar 2-nel volt gondom :(

--- Ellenorzofeladat/main.c
+++ Ellenorzofeladat/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
-{
-    printf("Hello world!\n");
+bool prim(int szam){
+    int oszto = 2;
+    bool primszam = true;
+    do {
+        if (szam % oszto == 0){
+            primszam = false;
+        }
+        oszto++;
+    }while (oszto < szam && primszam != false);
+
+    return (primszam);
+}
+
+
+int main(){
+    int szam;
+
+    printf("Addj meg egy 1-nel nagyobb egesz szamot: \n");
+    scanf("%d", &szam);
+
+    if (prim(szam) || szam == 2){
+        printf("Ez egy prim szam!");
+    }
+    else{
+        printf("Ez egy osszetett szam!");
+    }
     return 0;
 }
